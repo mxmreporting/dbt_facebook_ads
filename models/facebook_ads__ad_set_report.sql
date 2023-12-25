0a20280bb921c70561cddb879cf724536a5b384b
mxmreporting <142613858+mxmreporting@users.noreply.github.com>
2023-12-25 12:22:15 +0530

    Update facebook_ads__ad_set_report.sql

--- models/facebook_ads__ad_set_report.sql
+++ models/facebook_ads__ad_set_report.sql
@@ -1,4 +1,11 @@
-{{ config(enabled=var('ad_reporting__facebook_ads_enabled', True)) }}
+{{ config(enabled=var('ad_reporting__facebook_ads_enabled', True),
+    unique_key = ['source_relation','date_day','account_id','campaign_id','ad_set_id'],
+    partition_by={
+      "field": "date_day", 
+      "data_type": "date",
+      "granularity": "day"
+    }
+    ) }}
 
 with report as (
 
